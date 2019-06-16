56d3e5368e239acaebb7ccc534f1d07aa5378173
Nikolay Toporkov <toporkov.kolya96@gmail.com>
2019-06-16 16:35:06 +0300

    add links to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 import { logout as action } from '../actions/loginActions';
 
-const Header = ({ isLogged, logout }) =>
+const Header = ({ isLogged, isAdmin, logout }) =>
   isLogged && (
     <div>
+      <Link to="/user-list">Список работников</Link>
+      {isAdmin && <Link to="/add-user">Добавить нового работника</Link>}
+      <Link to="/report">Сгенерировать отчет</Link>
       <button type="button" onClick={logout}>
         Выход
       </button>
@@ -14,7 +18,8 @@ const Header = ({ isLogged, logout }) =>
 
 export default connect(
   state => ({
-    isLogged: Object.entries(state.user).length !== 0
+    isLogged: Object.entries(state.user).length !== 0,
+    isAdmin: state.user.isAdmin
   }),
   dispatch => ({
     logout: () => dispatch(action())
